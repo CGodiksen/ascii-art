a9dbf399feba3a4a0372c482e05dfc73ec836f51
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-05-21 18:46:05 +0200

    Add project files.

---
+++ AsciiArt/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+
+namespace AsciiArt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating the output directory.
+            System.IO.Directory.CreateDirectory("output");
+            
+            using (Image image = Image.Load("ascii-pineapple.jpg"))
+            {
+                Console.WriteLine("Height: " + image.Height + "\nWidth: " + image.Width);
+            }
+        }
+    }
+}
