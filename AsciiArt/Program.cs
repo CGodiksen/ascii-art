08f086980221b4287fea5b42cf5e847479329726
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-05-22 01:08:53 +0200

    Added a 2d array that contains the brightness of each pixel in the image.
    The brightness is computed by taking the average of the RBG values.

--- AsciiArt/Program.cs
+++ AsciiArt/Program.cs
@@ -12,12 +12,26 @@ namespace AsciiArt
 
             using (Bitmap image = new Bitmap("ascii-pineapple.jpg"))
             {
+                // Creating a 2d array that will contain the brightness for each pixel in the image.
+                int[,] brightness = new int[image.Height, image.Width];
+
                 Console.WriteLine("Height: " + image.Height + "\nWidth: " + image.Width);
                 for (int x = 0; x < image.Height; x++)
                 {
                     for (int y = 0; y < image.Width; y++)
                     {
-                        Console.WriteLine($"{x} x {y}: {image.GetPixel(y, x)}");
+                        // Computing the brightness of the pixel as the average of the RBG values.
+                        Color pixel = image.GetPixel(y, x);
+                        brightness[x, y] = (pixel.R + pixel.B + pixel.G) / 3;
+                        
+                    }
+                }
+                // Iterating through the 2d brightness array and printing each pixels brightness.
+                for (int i = 0; i < brightness.GetLength(0); i++)
+                {
+                    for (int j = 0; j < brightness.GetLength(1); j++)
+                    {
+                        Console.WriteLine(brightness[i, j]);
                     }
                 }
             }
