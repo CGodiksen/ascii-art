cdcd8f7db194eaa9394c3623006bc7502dcb8cf9
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-05-22 20:44:00 +0200

    Changed the design to a more object-oriented appoach.
    Created a class that represents an ASCII representation of an image. The object is constructed with the image that you wish to convert to ASCII and comes with a function to print the ASCII representation.

---
+++ AsciiArt/AsciiRepresentation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Drawing;
+
+namespace AsciiArt
+{
+    class AsciiRepresentation
+    {
+        private readonly Bitmap image;
+        private readonly Char[,] asciiRepresentation;
+        
+        public AsciiRepresentation(Bitmap image)
+        {
+            this.image = image;
+
+            // Initializing the ASCII representation of the image.
+            this.asciiRepresentation = InitializeAsciiArray();
+        }
+        
+        private char[,] InitializeAsciiArray()
+        {
+            // Creating a string that contains 65 characters ordered from thinnest to boldest.
+            string asciiChars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
+            
+            char[,] asciiArray = new char[image.Height, image.Width];
+            
+            // Iterating through every pixel in the image.
+            for (int x = 0; x < image.Height; x++)
+            {
+                for (int y = 0; y < image.Width; y++)
+                {
+                    // Computing the brightness of the pixel as the average of the RBG values.
+                    int pixel_brightness = GetBrightness(image.GetPixel(y, x));
+
+                    // Assigning an ASCII character to the pixel based on the brightness.
+                    if (pixel_brightness == 0 || (65 / (255 / pixel_brightness)) - 1 == -1)
+                    {
+                        asciiArray[x, y] = asciiChars[0];
+                    }
+                    else
+                    {
+                        asciiArray[x, y] = asciiChars[(65 / (255 / pixel_brightness)) - 1];
+                    }
+                }
+            }
+            return asciiArray;
+        }
+
+        private int GetBrightness(Color pixel)
+        {
+            return (pixel.R + pixel.B + pixel.G) / 3;
+        }
+        
+        // Printing the ASCII representation of the image.
+        public void PrintAsciiRepresentation()
+        {
+            for (int i = 0; i < asciiRepresentation.GetLength(0); i++)
+            {
+                for (int j = 0; j < asciiRepresentation.GetLength(1); j++)
+                {
+                    Console.Write(asciiRepresentation[i, j]);
+                }
+                Console.Write("\n");
+            }
+        }
+    }
+}
--- AsciiArt/Program.cs
+++ AsciiArt/Program.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Drawing;
+﻿using System.Drawing;
 
 namespace AsciiArt
 {
@@ -10,42 +9,10 @@ namespace AsciiArt
             // Resizing the image.
             Bitmap original = (Bitmap)Image.FromFile("ascii-pineapple.jpg");
             Bitmap image = new Bitmap(original, new Size(original.Width / 3, original.Height / 3));
-                
-            // Creating a string that contains 65 characters ordered from thinnest to boldest.
-            string asciiChars = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
-                
-            // Creating a 2d array that will contain the ASCII representation of the image.
-            char[,] asciiRepresentation = new char[image.Height, image.Width];
 
-            // Iterating through every pixel in the image.
-            for (int x = 0; x < image.Height; x++)
-            {
-                for (int y = 0; y < image.Width; y++)
-                {
-                    // Computing the brightness of the pixel as the average of the RBG values.
-                    Color pixel = image.GetPixel(y, x);
-                    int pixel_brightness = (pixel.R + pixel.B + pixel.G) / 3;
+            AsciiRepresentation asciiRepresentation = new AsciiRepresentation(image);
 
-                    // Assigning an ASCII character to the pixel based on the brightness.
-                    if (pixel_brightness == 0 || (65 / (255 / pixel_brightness)) - 1 == -1)
-                    {
-                        asciiRepresentation[x, y] = asciiChars[0];
-                    }
-                    else
-                    {
-                        asciiRepresentation[x, y] = asciiChars[(65 / (255 / pixel_brightness)) - 1];
-                    }
-                }
-            }
-            // Printing the ASCII representation of the image.
-            for (int i = 0; i < asciiRepresentation.GetLength(0); i++)
-            {
-                for (int j = 0; j < asciiRepresentation.GetLength(1); j++)
-                {   
-                    Console.Write(asciiRepresentation[i, j]);
-                }
-                Console.Write("\n");
-            }
+            asciiRepresentation.PrintAsciiRepresentation();
         }
     }
 }
