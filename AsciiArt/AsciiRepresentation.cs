666ae59342b78f17e33c200d93351d95eb8afc9b
CGodiksen <36046286+CGodiksen@users.noreply.github.com>
2020-05-23 01:21:06 +0200

    Added an option to invert the brightness.

--- AsciiArt/AsciiRepresentation.cs
+++ AsciiArt/AsciiRepresentation.cs
@@ -35,7 +35,7 @@ namespace AsciiArt
             {
                 for (int y = 0; y < image.Width; y++)
                 {
-                    int pixel_brightness = GetBrightness(image.GetPixel(y, x), MappingMethod.Average);
+                    int pixel_brightness = GetBrightness(image.GetPixel(y, x), MappingMethod.Average, true);
 
                     // Assigning an ASCII character to the pixel based on the brightness.
                     if (pixel_brightness == 0 || (65 / (255 / pixel_brightness)) - 1 == -1)
@@ -51,22 +51,29 @@ namespace AsciiArt
             return asciiArray;
         }
 
-        // Maps the RGB value of the pixel to a single brightness value based on the given mapping method. 
-        private int GetBrightness(Color pixel, MappingMethod mappingMethod)
-        {   
+        // Maps the RGB value of the pixel to a single brightness value based on the given mapping method. The value is inverted if invert is true. 
+        private int GetBrightness(Color pixel, MappingMethod mappingMethod, bool invert = false)
+        {
+            int brightness;
+
             if (mappingMethod.Equals(MappingMethod.Luminosity))
             {
-                return (int)(0.21 * pixel.R + 0.72 * pixel.G + 0.07 * pixel.B);
+                brightness = (int)(0.21 * pixel.R + 0.72 * pixel.G + 0.07 * pixel.B);
             }
             else if (mappingMethod.Equals(MappingMethod.MinMax))
             {   
-                return (Math.Max(pixel.R, Math.Max(pixel.B, pixel.G)) + Math.Min(pixel.R, Math.Min(pixel.B, pixel.G))) / 2;
+                brightness = (Math.Max(pixel.R, Math.Max(pixel.B, pixel.G)) + Math.Min(pixel.R, Math.Min(pixel.B, pixel.G))) / 2;
             }
             else
             {   
-                return (pixel.R + pixel.B + pixel.G) / 3;
+                brightness = (pixel.R + pixel.B + pixel.G) / 3;
             }
-           
+            
+            if (invert)
+            {
+                brightness = Math.Abs(brightness - 255);
+            }
+            return brightness;
         }
         
         // Printing the ASCII representation of the image.
